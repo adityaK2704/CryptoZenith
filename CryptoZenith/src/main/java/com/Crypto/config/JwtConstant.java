e60e29b92230c4b1296e5f02d0c774e6a534110f
adityakulkarni <adk27042000@gmail.com>
2024-08-25 15:25:14 +0530

    commit

---
+++ CryptoZenith/src/main/java/com/Crypto/config/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.Crypto.config;
+
+public class JwtConstant {
+    public static final String SECRETE_KEY="dffsfuihfhjodhjfoihojdflohjfosdnofnlnlndvknfdnfihfkdnkdkfnkdf";
+
+    public static final String JWT_HEADER="Authorization";
+}
